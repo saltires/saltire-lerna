8fefcd2f2c935f07bda9c6f0920f1b6e07c7c4d0
saltires <ye998102@163.com>
2021-11-25 09:33:44 +0800

    feat: 更改war模板

--- packages/war/deploy/scripts/install.sh
+++ packages/war/deploy/scripts/install.sh
@@ -2,4 +2,6 @@
 #!/bin/sh
 # TIME_STAMP=$(date "+%Y%m%d%H%M%S")
 # cp -a ${webroot}/pub ./pub-bak-$TIME_STAMP
-rm -rf ${workspace}/${appName} unzip -n ${workspace}/tmp/${appName}-1.*.war -d ${workspace}/${appName} rm -rf ${workspace}/tmp/${appName}-1.*.war
+rm -rf ${workspace}/${appName} 
+unzip -n ${workspace}/tmp/${appName}-1.*.war -d ${workspace}/${appName} 
+rm -rf ${workspace}/tmp/${appName}-1.*.war
